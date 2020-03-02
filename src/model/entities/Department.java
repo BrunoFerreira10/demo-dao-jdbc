56decb205670e9ab85f2ddced25129bd32698cfc
Bruno Ferreira <brunofelipe.f1@gmail.com>
2020-03-02 20:55:53 -0300

    findById implementation

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -7,12 +7,12 @@ public class Department implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
-	private Integer name;
+	private String name;
 	
 	public Department() {
 	}
 	
-	public Department(Integer id, Integer name) {
+	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
@@ -25,11 +25,11 @@ public class Department implements Serializable{
 		this.id = id;
 	}
 
-	public Integer getName() {
+	public String getName() {
 		return name;
 	}
 
-	public void setName(Integer name) {
+	public void setName(String name) {
 		this.name = name;
 	}
 
